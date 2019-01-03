eaba19aad7df8288ca6d2c4b2c88759ff1d6988f
Shloimi Minkowitz <shloimi.minkowitz@chabad.org>
2019-01-03 11:33:04 -0600

    Updates

---
+++ precache-manifest.3a83c49d36a58a801121602196ebcbe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "b55cb8f2b16138ff9012",
+    "url": "/contactmanager/static/js/main.b55cb8f2.chunk.js"
+  },
+  {
+    "revision": "2c9a28f9a441276d7e30",
+    "url": "/contactmanager/static/js/1.2c9a28f9.chunk.js"
+  },
+  {
+    "revision": "b55cb8f2b16138ff9012",
+    "url": "/contactmanager/static/css/main.162e99f4.chunk.css"
+  },
+  {
+    "revision": "2c9a28f9a441276d7e30",
+    "url": "/contactmanager/static/css/1.c030eddb.chunk.css"
+  },
+  {
+    "revision": "876b2e10681ad5ba721c36863ab7d3ca",
+    "url": "/contactmanager/index.html"
+  }
+];
